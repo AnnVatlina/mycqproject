0fd29d71490b0de2f84019f73bff07de3cd710c6
AnnVatlina <ann.vatlina@gmail.com>
2015-02-17 13:04:26 +0300

    add get weather

--- bundle/src/main/java/com/axamit/training/mycqproject/service/impl/WeatherServiceImpl.java
+++ bundle/src/main/java/com/axamit/training/mycqproject/service/impl/WeatherServiceImpl.java
@@ -9,6 +9,13 @@ import org.apache.felix.scr.annotations.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * <p>Copyright (c) 2014 Axamit</p>
  * User: anna.vatlina on 16.02.2015, 10:33
@@ -17,15 +24,45 @@ import org.slf4j.LoggerFactory;
 @Component
 @Service(value = WeatherService.class)
 @Properties({
-        @Property(name = "adaptables", value = {"Resource"}),
-        @Property(name = "adapters", value = {"WeatherModel"})
+        @Property(name = "resourcePath", value = {"http://api.openweathermap.org/data/2.5/weather"})
 })
 public class WeatherServiceImpl implements WeatherService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(WeatherServiceImpl.class);
+    private static final String targetURL = "http://api.openweathermap.org/data/2.5/weather?q=London,uk";
 
     @Override
     public void doSomething(WeatherModel weatherModel) {
-        System.out.println("WeatherServiceImpl.doSomething");
+        LOGGER.info("WeatherServiceImpl.doSomething");
+        try {
+
+            URL restServiceURL = new URL(targetURL);
+
+            HttpURLConnection httpConnection = (HttpURLConnection) restServiceURL.openConnection();
+            httpConnection.setRequestMethod("GET");
+            httpConnection.setRequestProperty("Accept", "application/json");
+
+            if (httpConnection.getResponseCode() != 200) {
+                throw new RuntimeException("HTTP GET Request Failed with Error code : "
+                        + httpConnection.getResponseCode());
+            }
+
+            BufferedReader responseBuffer = new BufferedReader(new InputStreamReader(
+                    (httpConnection.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server:  \n");
+
+            while ((output = responseBuffer.readLine()) != null) {
+                LOGGER.info(output);
+            }
+
+            httpConnection.disconnect();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- content/src/main/content/jcr_root/apps/mycqproject/components/weather/weather.jsp
+++ content/src/main/content/jcr_root/apps/mycqproject/components/weather/weather.jsp
@@ -1,6 +1,5 @@
 <%@ page import="com.axamit.training.mycqproject.models.WeatherModel" %>
 <%@include file="/libs/foundation/global.jsp"%>
-
 <div class="weather_block">
 
     <h3>${properties.titleWeatherComponent}</h3>
@@ -12,11 +11,10 @@
     ${properties.cityWeatherComponents}
     ${properties.unitWeatherComponents}
 
-    ${sling:adaptTo(resource, "com.axamit.training.mycqproject.models.WeatherModel")}
+    <%--${sling:adaptTo(resource, "com.axamit.training.mycqproject.models.WeatherModel")}--%>
+    <%--<sling:adaptTo adaptable='${resource}' adaptTo='com.axamit.training.mycqproject.models.WeatherModel' var="model"/>--%>
     <%
-        /*WeatherModel weatherModel = resource.adaptTo(WeatherModel.class);
-        System.out.println("JspClass.jsp_service_method");
-        weatherModel.getCityWeatherComponents();*/
+        WeatherModel weatherModel = resource.adaptTo(WeatherModel.class);
     %>
 
 </div>
